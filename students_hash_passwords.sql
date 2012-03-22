dd9dc50cae7214d3b477d478dc4651ea0111dd67
Ryan Ammons <ryan.ammons@celticwolf.com>
2012-03-21 21:36:46 -0400

    Added script to update the plain-text passwords in all records of the student table with the properly hashed value. Issue #25

---
+++ students_hash_passwords.sql
@@ -0,0 +1,9 @@
+-- Issue #25
+-- Update existing, plain-text passwords in the 'students'
+-- table to hashed values in order to match changes in the 
+-- PHP authentication code. 
+-- Specifically, the PHP code hashes the user's password 
+-- input with crypt(password,'lF'). The equivalent hashing 
+-- function in MySQL is ENCRYPT. The same salt value is used.
+
+UPDATE `students` SET `password` = ENCRYPT(`password`,'lF');

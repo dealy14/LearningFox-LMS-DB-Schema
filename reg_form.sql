bfb2a9cc6b20d854070bdf31183e7323a0920535
Jerry Stuckle <jerry@smartechhomes.com>
2012-03-26 19:46:09 -0400

    Added new fields to students table and changed reg_form to display the new fields, per Issue #60.

---
+++ reg_form.sql
@@ -0,0 +1,6 @@
+INSERT INTO `reg_form` (`field_name`,`status`,`forder`,`stored`,`display`)
+    VALUES ('organization', 'on', '13', 'y', 'Organization'), 
+           ('location', 'on', '14', 'y', 'Location'), 
+           ('domain', 'on', '15', 'y', 'Domain'), 
+           ('manger', 'on', '16', 'y', 'Manager');
+
--- students.sql
+++ students.sql
@@ -1,40 +1,5 @@
--- Issue 27
--- Add default empty default values for columns
--- in 'students' table.
--- This might also work out if we made the columns
--- NULLable, but unsure how it might affect the rest
--- of the code, so just using a blank default.
--- Also, the default charset/collation for the columns
--- is latin1/latin1_swedish_ci, while some other tables have
--- UTF8/utf8_general_ci.  This should be standardized
--- over the entire dataset.
-
-ALTER TABLE `students` 
-  CHANGE `confirmation_number` 
-    `confirmation_number` 
-    VARCHAR(255) 
-    CHARACTER SET latin1 
-    COLLATE latin1_swedish_ci 
-    NOT NULL 
-    DEFAULT '', 
-  CHANGE `organization_name` 
-    `organization_name` 
-    VARCHAR(255) 
-    CHARACTER SET latin1 
-    COLLATE latin1_swedish_ci 
-    NOT NULL 
-    DEFAULT '', 
-  CHANGE `last_login` 
-    `last_login` 
-    VARCHAR(255) 
-    CHARACTER SET latin1 
-    COLLATE latin1_swedish_ci 
-    NOT NULL 
-    DEFAULT '', 
-  CHANGE `reg_date` 
-    `reg_date` 
-    VARCHAR(255) 
-    CHARACTER SET latin1 
-    COLLATE latin1_swedish_ci 
-    NOT NULL 
-    DEFAULT '';
+ALTER TABLE `students`  
+    ADD `organization` VARCHAR(50) NOT NULL DEFAULT '',  
+    ADD `location` VARCHAR(50) NOT NULL DEFAULT '',
+    ADD `domain` VARCHAR(50) NOT NULL DEFAULT '',
+    ADD `manager` VARCHAR(50) NOT NULL DEFAULT '';

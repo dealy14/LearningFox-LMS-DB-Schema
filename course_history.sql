2aa306399ffb7c4e5bce13669dd1b84b07fb761f
Ryan Ammons <ryan@ammonsdatasolutions.com>
2014-04-10 16:51:42 -0400

    Added 'enroll_date' field for expiry calculation
    The enroll_date will generally be filled by the API with the current date when a course is purchased. It will be used to calculate course expiration.

--- course_history.sql
+++ course_history.sql
@@ -3,3 +3,9 @@ ALTER TABLE course_history
   lesson_location VARCHAR( 255 )
    NOT NULL 
   DEFAULT '';
+
+ALTER TABLE  course_history
+  ADD enroll_date 
+  DATE NULL 
+  DEFAULT  '0000-00-00' 
+  AFTER  start_date;
